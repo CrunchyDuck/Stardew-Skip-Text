b2d437b20baaba599378f760f631ed3f76e66e1a
CrunchyDuck <43281440+CrunchyDuck@users.noreply.github.com>
2021-06-17 04:22:15 +0100

    inital commit

---
+++ README.md
@@ -0,0 +1,5 @@
+makes text fast in stardew
+
+More information can be found [here.](https://www.nexusmods.com/stardewvalley/mods/8947)
+
+A note on compilation of my code: I used the Pathoschild Stardew NuGet package, which I have not included in this repository.
---
+++ SkipTextScroll/SkipText.cs
@@ -0,0 +1,25 @@
+﻿using StardewModdingAPI;
+using Harmony;
+
+namespace SkipText {
+    public class ModEntry : Mod {
+        public override void Entry(IModHelper helper) {
+            //Patch.Init(this);
+            var harmony = HarmonyInstance.Create(this.ModManifest.UniqueID);
+            System.Reflection.MethodInfo original = AccessTools.Method(typeof(StardewValley.Menus.DialogueBox), "update"); // Handles the dialogue and text boxes
+            HarmonyMethod patch = new HarmonyMethod(typeof(Patch), "Prefix");
+            harmony.Patch(original, prefix: patch);
+        }
+    }
+
+    class Patch {
+  //      private static ModEntry Mod;
+  //      public static void Init(ModEntry mod) {
+  //          Mod = mod;
+		//}
+
+        public static void Prefix(StardewValley.Menus.DialogueBox __instance) {
+            __instance.characterIndexInDialogue = __instance.getCurrentString().Length;
+        }
+    }
+}
